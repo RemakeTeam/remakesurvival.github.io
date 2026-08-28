e46a70c77d84585c3e7a678b7ce3402ebcb8e5eb
ManToon <vol4onok175@gmail.com>
2026-08-28 22:36:42 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 export function openUrl(url) {
     window.open(url, "_blank");
 }
+
+export function getPath() {
+    return window.location.pathname;
+}
+
+export function navigate(path) {
+    window.history.pushState({}, "", path);
+}
+
+export function setTitle(title) {
+    document.title = title;
+}
